7a184d8a1783d4e2b1e35cba5c3716102d62a755
Joseph Lev-Tov <jtlevtov@gmail.com>
2022-04-01 14:35:59 -0600

    ?

---
+++ FPS-Game/Assets/Scripts/sound.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class sounds : MonoBehaviour {
+    public AudioSource gamersound;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
